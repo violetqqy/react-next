c31595debe18fc9f8c7feaf795fa4a9b205c4ad2
VioletQQY <forviolet771@gmail.com>
2018-04-10 17:30:44 +0800

    modify

--- components/Account/AccountAdd.js
+++ components/Account/AccountAdd.js
@@ -1,82 +1,69 @@
 import react from 'react'
 import Link from 'next/link'
 import {
+  InputItem,
   List,
-  Icon
+  Picker
 } from 'antd-mobile'
 
-class AccountList extends React.Component {
+class AccountAdd extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      avatar: '',
+      name: '',
+      telephone: '',
+      call: '',
+      callList: [
+        [{
+          value: '父亲',
+          label: '父亲'
+        }, {
+          value: '母亲',
+          label: '母亲'
+        }, {
+          value: '丈夫',
+          label: '丈夫'
+        }]
+      ]
+    }
+    this.onTelephoneChange = this.onTelephoneChange.bind(this)
+  }
+
+  onTelephoneChange(value) {
+    console.log(value)
+    this.setState({
+      telephone: value
+    })
   }
 
   render() {
     return (
       <div>
-        <div className='box'>
-          <div className='middle-wrap'>
-            <div className='middle'>
-              {this.props.name}
-            </div>
-          </div>
-          <div className='left'>
-            <img className='avatar' src={this.props.avatar} />
-          </div>
-          {
-            this.props.status == '申请中' ? (
-              <div className='right red'>
-                {this.props.status}
-              </div>
-            ) : (
-              <div className='right'>
-                {this.props.status}
-              </div>
-            )
-          }
+        <div className='card'>
+          <img src={this.state.avatar} />
+          <span>{this.state.name}</span>
         </div>
+        <div>
+          <p>1.他/她的手机号</p>
+          <InputItem 
+            type='phone' 
+            onChange={this.onTelephoneChange} 
+            value={this.state.telephone} />
+        </div>
+        <List>
+          <Picker 
+            extra='请选择'
+            data={this.state.callList}
+            cascade={false}
+            title='您的选择'
+            value={this.state.call}
+            onOk={e => console.log('ok', e)}
+            onDismiss={e => console.log('dismiss', e)}>
+            <List.Item arrow='horizontal'>{this.state.call}</List.Item>
+          </Picker>
+        </List>
         <style jsx>{`
-          .box {
-            position: relative;
-            height: 36px;
-            line-height: 36px;
-            color: #666;
-            font-size: 15px;
-          }
-          .middle-wrap {
-            position: relative;
-            float: left;
-            width: 100%;
-            height: 100%;
-          }
-          .middle-wrap .middle {
-            height: 100%;
-            margin: 0 100px 0 50px;
-            text-algin: left;
-          }
-          .left {
-            float: left;
-            width: 50px;
-            margin-left: -100%;
-            height: 100%;
-          }
-          .right {
-            float: left;
-            width: 100px;
-            height: 100%;
-            margin-left: -100px;
-            font-size: 14px;
-            text-align: right;
-            color: #999;
-          }
-          .avatar {
-            display: block;
-            width: 36px;
-            height: 36px;
-            border-radius: 36px;
-          }
-          .red {
-            color: #da322e;
-          }
         `}
         </style>
       </div>
@@ -84,43 +71,4 @@ class AccountList extends React.Component {
   }
 }
 
-class FamilyAccountAccount extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <div>
-        <List>{
-          Array.isArray(this.props.familyList) ? 
-          this.props.familyList.map((obj, i) => (
-            <List.Item  key={i}>
-              <AccountList key={i} name={obj.name} status={obj.status} avatar={obj.avatar} />
-            </List.Item>
-          )) : ''
-          }
-          <List.Item>
-            <Link href='/account/add'>
-              <div className='item-add'>添加新家庭成员</div>
-            </Link>
-          </List.Item>
-        </List>
-        <style jsx>{`
-          .item-add {
-            width: 100%;
-            padding: 45px 0 0 0;
-            margin: 10px;
-            background: url(/static/images/icon-add.png) top center no-repeat;
-            background-size: 35px 35px;
-            color: #999;
-            font-size: 16px;
-            text-align: center;
-          }
-        `}</style>
-      </div>
-    )
-  }
-}
-
-export default FamilyAccountList
+export default AccountAdd
--- pages/account/add.js
+++ pages/account/add.js
@@ -1,5 +1,16 @@
-export default () => (
-  <div>
-    <p>add</p>
-  </div>
-)
+import React from 'react'
+import AccountAdd from '../../components/Account/AccountAdd'
+
+export default class extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <AccountAdd />
+      </div>
+    )
+  }
+}
